9bd65e4a60503fb0b340c6be6f5d6c2f1f257df9
ellieisjelly <ellie@ellieis.me>
2026-08-27 17:36:26 -0300

    Consertar css errado, suporte para PDF no cadastro de projeto

--- scripts/cadastro-projeto.js
+++ scripts/cadastro-projeto.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const dropZone = document.getElementById("dropZone");
-    const fileInput = document.getElementById("fileInput");
-    const fileList = document.getElementById("fileList");
-
-    dropZone.addEventListener("click", () => fileInput.click());
+    const dropZoneImage = document.getElementById("dropZoneImage");
+    const fileInputImage = document.getElementById("fileInputImage");
+    const fileListImage = document.getElementById("fileListImage");
+    const dropZonePDF = document.getElementById("dropZonePDF");
+    const fileInputPDF = document.getElementById("fileInputPDF");
+    const fileListPDF = document.getElementById("fileListPDF");
+    dropZoneImage.addEventListener("click", () => fileInputImage.click());
+    dropZonePDF.addEventListener("click", () => fileInputPDF.click());
 
     ["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
-        dropZone.addEventListener(eventName, preventDefaults, false);
+        dropZoneImage.addEventListener(eventName, preventDefaults, false);
+        dropZonePDF.addEventListener(eventName, preventDefaults, false);
     });
 
     function preventDefaults(e) {
@@ -15,57 +19,109 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     ["dragenter", "dragover"].forEach((eventName) => {
-        dropZone.addEventListener(
+        dropZoneImage.addEventListener(
             eventName,
             () => {
-                dropZone.classList.add("drag-over");
+                dropZoneImage.classList.add("drag-over");
+            },
+            false,
+        );
+        dropZonePDF.addEventListener(
+            eventName,
+            () => {
+                dropZonePDF.classList.add("drag-over");
             },
             false,
         );
     });
 
     ["dragleave", "drop"].forEach((eventName) => {
-        dropZone.addEventListener(
+        dropZoneImage.addEventListener(
             eventName,
             () => {
-                dropZone.classList.remove("drag-over");
+                dropZoneImage.classList.remove("drag-over");
+            },
+            false,
+        );
+        dropZonePDF.addEventListener(
+            eventName,
+            () => {
+                dropZonePDF.classList.remove("drag-over");
             },
             false,
         );
     });
 
-    dropZone.addEventListener("drop", (e) => {
+    dropZoneImage.addEventListener("drop", (e) => {
         const dt = e.dataTransfer;
         const files = dt.files;
 
         if (files.length) {
-            handleFiles(files);
+            handleFiles(files, true);
         }
     });
 
-    fileInput.addEventListener("change", function () {
-        if (this.files.length) {
-            handleFiles(this.files);
+    dropZonePDF.addEventListener("drop", (e) => {
+        const dt = e.dataTransfer;
+        const files = dt.files;
+
+        if (files.length) {
+            handleFiles(false);
         }
     });
-    const allowedExtensions = [".png", ".jpg", ".jpeg"];
-    function handleFiles(files) {
+
+    fileInputImage.addEventListener("change", function () {
+        if (this.files.length) {
+            handleFiles(this.files, true);
+        }
+    });
+
+    fileInputPDF.addEventListener("change", function () {
+        if (this.files.length) {
+            handleFiles(this.files, false);
+        }
+    });
+
+    const allowedExtensionsImage = [".png", ".jpg", ".jpeg"];
+    const allowedExtensionsPDF = [".pdf"];
+
+    function handleFiles(files, isImage) {
+        let fileList;
+        let validExtensions;
+        let maxSize;
+        if (isImage) {
+            fileList = fileListImage;
+            validExtensions = "PNG, JPG ou JPEG";
+            maxSize = 1024 * 10;
+        } else {
+            validExtensions = "PDF";
+            fileList = fileListPDF;
+            maxSize = 1024 * 50;
+        }
         fileList.innerHTML = "";
         const file = files[0];
         const fileName = file.name.toLowerCase();
-        const hasValidExtension = allowedExtensions.some((ext) =>
-            fileName.endsWith(ext),
-        );
+        let hasValidExtension;
+        if (isImage) {
+            hasValidExtension = allowedExtensionsImage.some((ext) =>
+                fileName.endsWith(ext),
+            );
+        } else {
+            hasValidExtension = allowedExtensionsPDF.some((ext) =>
+                fileName.endsWith(ext),
+            );
+        }
+
         if (!hasValidExtension) {
             alert(
-                "Formato de arquivo inválido. Por favor, envie um arquivo PNG, JPG ou JPEG.",
+                `Formato de arquivo inválido. Por favor, envie um arquivo ${validExtensions}.`,
             );
             return;
         }
         const size = (file.size / 1024).toFixed(1);
-        if (size > 1024 * 10) {
+        if (size > maxSize) {
             alert(
-                "O arquivo é muito grande. Por favor, envie um arquivo menor que 10 MB.",
+                `O arquivo é muito grande. Por favor, envie um arquivo menor que ${maxSize / 1024} MB.`,
             );
             return;
         }
